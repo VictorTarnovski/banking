b814b69c53e0e899123eaef466cbeeb45d7f7b91
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-07-14 12:20:59 -0300

    feat: add presentation layer

---
+++ presentation/src/main/java/com/victor_tarnovski/banking/presentation/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.victor_tarnovski.banking.presentation.controllers;
+
+import com.victor_tarnovski.banking.domain.dtos.RegisterUserDTO;
+import com.victor_tarnovski.banking.application.use_cases.RegisterUserUseCase;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/v1/auth")
+@Produces(MediaType.APPLICATION_JSON)
+public class AuthController {
+
+  private final RegisterUserUseCase useCase;
+
+  @Inject
+  AuthController(final RegisterUserUseCase useCase) {
+    this.useCase = useCase;
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response register(final RegisterUserDTO dto) {
+    useCase.execute(dto);
+    return Response.ok().build();
+  }
+
+}
