e6fb65bc9d2e40bdc251a9d86abcc12b07c7b923
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-07-30 12:18:38 -0300

    feat: add request / response logs to TransferAuthorizerAdapter

--- application/src/main/java/com/victor_tarnovski/banking/application/adapters/TransferAuthorizerAdapter.java
+++ application/src/main/java/com/victor_tarnovski/banking/application/adapters/TransferAuthorizerAdapter.java
@@ -6,7 +6,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.victor_tarnovski.banking.application.exceptions.UnauthorizedTransferException;
 import com.victor_tarnovski.banking.domain.aggregates.Wallet;
@@ -14,20 +16,27 @@ import com.victor_tarnovski.banking.domain.ports.TransferAuthorizer;
 import com.victor_tarnovski.banking.domain.vo.Money;
 
 public class TransferAuthorizerAdapter implements TransferAuthorizer {
+
+  private static final Logger log = LoggerFactory.getLogger(TransferAuthorizerAdapter.class);
+  
   @Override
   public void authorize(Wallet fromWallet, Wallet toWallet, Money amount) {
     // Simulating an external authorization service call
     // In a real application, this would be replaced with actual logic to check authorization
     HttpClient client = HttpClient.newHttpClient();
+    URI uri = URI.create("https://util.devi.tools/api/v2/authorize");
     HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://util.devi.tools/api/v2/authorize"))
+        .GET()
+        .uri(uri) 
         .build();
+
+    log.info(request.toString());
 
     try {
       HttpResponse<String> response= client.send(request, BodyHandlers.ofString());
-      var pattern = Pattern.compile("\"authorization\" : true");
-      var matcher = pattern.matcher(response.body());
-      if (!matcher.find()) {
+      log.info(response.toString());
+      log.info(response.body());
+      if(response.statusCode() != 200) {
         throw new UnauthorizedTransferException();
       }
     } catch (IOException | InterruptedException e) {
