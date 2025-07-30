7a2d2ef1048ed399bf3096a445e7056b33fa198f
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2025-07-30 11:32:35 -0300

    feat: add state change log to TransferAuthorizerCircuitBreaker.java

--- application/src/main/java/com/victor_tarnovski/banking/application/circuit_breakers/TransferAuthorizerCircuitBreaker.java
+++ application/src/main/java/com/victor_tarnovski/banking/application/circuit_breakers/TransferAuthorizerCircuitBreaker.java
@@ -2,6 +2,10 @@ package com.victor_tarnovski.banking.application.circuit_breakers;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
 import com.victor_tarnovski.banking.application.adapters.TransferAuthorizerAdapter;
 import com.victor_tarnovski.banking.application.enums.CircuitBreakerState;
 import com.victor_tarnovski.banking.application.exceptions.UnauthorizedTransferException;
@@ -20,11 +24,22 @@ public class TransferAuthorizerCircuitBreaker implements TransferAuthorizer {
   private final long retryAfterEach = 5;
   private CircuitBreakerState state = CircuitBreakerState.CLOSED;
 
+  private static final Logger log = LoggerFactory.getLogger(TransferAuthorizerCircuitBreaker.class);
+
+  public TransferAuthorizerCircuitBreaker() {
+    log.info("TransferAuthorizerCircuitBreaker initialized with state: {}", state);
+  }
+  
+  @Override
   public void authorize(Wallet fromWallet, Wallet toWallet, Money amount) {
+    CircuitBreakerState prevState = null;
+
     if (state == CircuitBreakerState.OPEN) {
       fails++;
       if ((fails % retryAfterEach) == 0) {
+        prevState = state;
         state = CircuitBreakerState.HALF_OPEN;
+        logStateChange(prevState, state, Level.INFO);
       } else {
         throw new UnauthorizedTransferException(
           new Throwable("circuit breaker is open, cannot authorize transfer")
@@ -34,9 +49,10 @@ public class TransferAuthorizerCircuitBreaker implements TransferAuthorizer {
 
     if (state == CircuitBreakerState.HALF_OPEN) { 
       try {
-        var authorizer = new TransferAuthorizerAdapter();
-        authorizer.authorize(fromWallet, toWallet, amount);
+        checkAuthorization(fromWallet, toWallet, amount);
+        prevState = state;
         state = CircuitBreakerState.CLOSED; 
+        logStateChange(prevState, state, Level.INFO);
         fails = 0; 
         return;
       } catch (UnauthorizedTransferException e) {
@@ -47,7 +63,9 @@ public class TransferAuthorizerCircuitBreaker implements TransferAuthorizer {
         ) {
           fails++;
           if (fails == maxFails) {
+            prevState = state;
             state = CircuitBreakerState.OPEN;
+            logStateChange(prevState, state, Level.WARN);
           }
           throw e;
         }
@@ -56,8 +74,7 @@ public class TransferAuthorizerCircuitBreaker implements TransferAuthorizer {
 
     if (state == CircuitBreakerState.CLOSED) {
       try {
-        var authorizer = new TransferAuthorizerAdapter();
-        authorizer.authorize(fromWallet, toWallet, amount);
+        checkAuthorization(fromWallet, toWallet, amount);
       } catch (UnauthorizedTransferException e) {
         var cause = e.getCause();
         if (
@@ -66,11 +83,31 @@ public class TransferAuthorizerCircuitBreaker implements TransferAuthorizer {
         ) {
           fails++;
           if (fails == maxFails) {
+            prevState = state;
             state = CircuitBreakerState.OPEN;
+            logStateChange(prevState, state, Level.WARN);
           }
           throw e;
         }
       }
     }
+
   }
+
+  private void checkAuthorization(Wallet fromWallet, Wallet toWallet, Money amount) {
+    var authorizer = new TransferAuthorizerAdapter();
+    authorizer.authorize(fromWallet, toWallet, amount);
+  }
+
+  private void logStateChange(
+    CircuitBreakerState prevState, 
+    CircuitBreakerState currState,
+    Level level
+  ) {
+    var message = String.format("TransferAuthorizerCircuitBreaker state changed from {} to {}", prevState, currState);
+    log.makeLoggingEventBuilder(level)
+      .setMessage(message)
+      .log();
+  } 
+
 }
